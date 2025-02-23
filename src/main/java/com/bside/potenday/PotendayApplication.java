788b7b0357fd7522d9917c1bd7e8b4dfc195ad4d
mjlog <62366330+bbc719@users.noreply.github.com>
2025-02-23 15:01:47 +0900

    Apply .gitignore settings

---
+++ src/main/java/com/bside/potenday/PotendayApplication.java
@@ -0,0 +1,13 @@
+package com.bside.potenday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotendayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PotendayApplication.class, args);
+	}
+
+}
